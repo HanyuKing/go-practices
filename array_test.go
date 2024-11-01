5b5c2a7016a662d39fe03c8b5e1b4694169ab9d1
Rain <hanyuking2016@gmail.com>
2024-11-01 17:25:02 +0800

    test

--- array_test.go
+++ array_test.go
@@ -22,3 +22,20 @@ func Test_ArrayNil(t *testing.T) {
 	fmt.Println(&b)
 	fmt.Println(&bb)
 }
+
+func Test_Append(t *testing.T) {
+	x := []int{1, 2, 3, 4, 5}
+	fmt.Println(len(x), cap(x))
+
+	x = append(x, 6)
+	fmt.Println(len(x), cap(x))
+
+	x = append(x, 7, 8, 9, 10)
+	y := x
+	y[0] = 111
+	fmt.Println(x, y, len(x), cap(x), len(y), cap(y))
+
+	x = append(x, 11)
+	y[0] = 11
+	fmt.Println(x, y, len(x), cap(x), len(y), cap(y))
+}
