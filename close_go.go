7ac7180b46919bf56dc1e22bfbcc6f93f866a93c
Rain <hanyuking2016@gmail.com>
2024-11-08 17:24:36 +0800

    update

--- close_go.go
+++ close_go.go
@@ -12,6 +12,7 @@ func main() {
 	//
 	//in := make(chan int)
 	//go func(in <-chan int) {
+	//	defer fmt.Println("worker exit")
 	//	// Using for-range to exit goroutine
 	//	// range has the ability to detect the close/end of a channel
 	//	for x := range in {
@@ -27,6 +28,7 @@ func main() {
 	//in := make(chan int)
 	//processedCnt := 0
 	//go func() {
+	//	defer fmt.Println("worker exit")
 	//	// in for-select using ok to exit goroutine
 	//	for {
 	//		select {
@@ -49,44 +51,45 @@ func main() {
 	//time.Sleep(time.Second)
 
 	// 多个通道关闭时
-	//in1 := make(chan int)
-	//in2 := make(chan int)
-	//go func() {
-	//	// in for-select using ok to exit goroutine
-	//	for {
-	//		select {
-	//		case x, ok := <-in1:
-	//			if !ok {
-	//				in1 = nil
-	//			}
-	//			// Process
-	//			fmt.Println(fmt.Sprintf("x: %d", x))
-	//		case y, ok := <-in2:
-	//			if !ok {
-	//				in2 = nil
-	//			}
-	//			// Process
-	//			fmt.Println(fmt.Sprintf("y: %d", y))
-	//		case <-time.After(time.Second):
-	//			fmt.Printf("Working. \n")
-	//		}
-	//
-	//		// If both in channel are closed, goroutine exit
-	//		if in1 == nil && in2 == nil {
-	//			fmt.Printf("Exit. \n")
-	//			return
-	//		}
-	//	}
-	//}()
-	//time.Sleep(5 * time.Second)
-	//in1 <- 1
-	//in2 <- 2
-	//
-	//close(in1)
-	//time.Sleep(time.Second)
-	//in2 <- 22
-	//time.Sleep(time.Second)
-	//close(in2)
+	in1 := make(chan int)
+	in2 := make(chan int)
+	go func() {
+		defer fmt.Println("worker exit")
+		// in for-select using ok to exit goroutine
+		for {
+			select {
+			case x, ok := <-in1:
+				if !ok {
+					in1 = nil
+				}
+				// Process
+				fmt.Println(fmt.Sprintf("x: %d", x))
+			case y, ok := <-in2:
+				if !ok {
+					in2 = nil
+				}
+				// Process
+				fmt.Println(fmt.Sprintf("y: %d", y))
+			case <-time.After(time.Second):
+				fmt.Printf("Working. \n")
+			}
+
+			// If both in channel are closed, goroutine exit
+			if in1 == nil && in2 == nil {
+				fmt.Printf("Exit. \n")
+				return
+			}
+		}
+	}()
+	time.Sleep(5 * time.Second)
+	in1 <- 1
+	in2 <- 2
+
+	close(in1)
+	time.Sleep(time.Second)
+	in2 <- 22
+	time.Sleep(time.Second)
+	close(in2)
 
 	// 3. 使用退出通道
 	//stopCh := make(chan struct{})
